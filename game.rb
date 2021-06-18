2d02ee7795d0cc7d64a8af631ad65d2006020b4f
mholmes <mholmes@email.com>
2021-06-18 21:56:21 +0100

    Get Name + Greeting

--- game.rb
+++ game.rb
@@ -4,37 +4,37 @@ input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
 
-puts "I've got a random number between 1 and 100."
-puts "Can you guess it?"
-target = rand(100)+1
+# puts "I've got a random number between 1 and 100."
+# puts "Can you guess it?"
+# target = rand(100)+1
 
-num_guesses = 0
+# num_guesses = 0
 
-guessed_it = false
+# guessed_it = false
 
-until num_guesses == 10 || guessed_it
+# until num_guesses == 10 || guessed_it
 
-puts "You've got #{10 - num_guesses} guesses left."
-print "Make a guess: "
-guess = gets.to_i
+# puts "You've got #{10 - num_guesses} guesses left."
+# print "Make a guess: "
+# guess = gets.to_i
 
-num_guesses += 1
+# num_guesses += 1
 
-if guess < target
-    puts "Oops. Your guess was LOW."
-elsif guess > target
-    puts "Oops. Your guess was HIGH."
-elsif guess == target 
-    puts "Good job, #{name}!"
-    puts "Your guesed my number in #{num_guesses} guesses!"
-    guessed_it = true
-end
+# if guess < target
+#     puts "Oops. Your guess was LOW."
+# elsif guess > target
+#     puts "Oops. Your guess was HIGH."
+# elsif guess == target 
+#     puts "Good job, #{name}!"
+#     puts "Your guesed my number in #{num_guesses} guesses!"
+#     guessed_it = true
+# end
 
-end
+# end
 
-unless guessed_it
-    puts "Sorry. You didn't get my number. (It was #{target}."
-end
+# unless guessed_it
+#     puts "Sorry. You didn't get my number. (It was #{target}."
+# end
 
 
 

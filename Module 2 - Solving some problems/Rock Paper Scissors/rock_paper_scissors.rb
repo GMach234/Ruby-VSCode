88b08cbfda564396a97826021b958c8ffc4bdbcf
mholmes <mholmes@email.com>
2021-07-04 20:59:57 +0100

    Loop game while invalid input or tie

--- Module 2 - Solving some problems/Rock Paper Scissors/rock_paper_scissors.rb
+++ Module 2 - Solving some problems/Rock Paper Scissors/rock_paper_scissors.rb
@@ -1,45 +1,53 @@
-
-#Start game - player input
-puts "Rock Paper Scissors"
-print "Player 1 - Enter your selection: "
-player1 = gets.chomp.downcase
-print "Player 2 - Enter your selection: "
-player2 = gets.chomp.downcase
-
 #Method to calculate result
 def calc_result(input1, input2)
 
     rpc = ['rock','paper','scissors']
-    if rpc.include?(input1) && rpc.include?(input2)
-    
+    if rpc.include?(input1) && rpc.include?(input2) 
         case input1
         when "rock"
             if input2 == "paper"
                 return "Paper covers rock.", "Player 2 wins."
             elsif input2 == "scissors"
                 return "Rock crushes scissors", "Player 1 wins."
-            else "Tie!"
-            end
+            else "Tie!" end
         when "paper"
             if input2 == "scissors"
                 return "Scissors cuts paper.", "Player 2 wins."
             elsif input2 == "rock"
                 return "Paper covers rock.", "Player 1 wins"
-            else "Tie!"
-            end
+            else "Tie!" end
         when "scissors"
             if input2 == "paper"
                 return "Scissors cuts paper.", "Player 1 wins."
             elsif input2 == "rock"
                 return "Rock crushes scissors", "Player 2 wins."
-            else "Tie!"
-            end
+            else "Tie!" end
         end 
-
-    else "No cheaters! Only Rock, Paper or Scissors are allowed."
+    else 
+        "No cheaters! Only Rock, Paper or Scissors are allowed."
     end
 end
 
-#Calculare result using method and print
-result = calc_result(player1,player2)
-puts result
+#Start game
+puts "Rock Paper Scissors"
+#Keep playing
+playAgain = true
+
+#Loop until game-end conditions
+until playAgain == false
+    print "Player 1 - Enter your selection: "
+    player1 = gets.chomp.downcase
+    print "Player 2 - Enter your selection: "
+    player2 = gets.chomp.downcase
+
+    #Calculare result using method and puts
+    result = calc_result(player1,player2)
+    puts result
+
+    #If results indicate bad input or a tie, play again
+    if result.include?("Tie") || result.include?("cheaters")
+        playAgain = true
+    else playAgain = false end
+end
+
+

425a793ee42e6ce4facf599bb13aba7ce3c87ed5
mholmes <mholmes@email.com>
2021-07-05 14:25:50 +0100

    the_repeating_loop where I want it

--- Module 3 - Arrays, Hashes, Blocks and Procs/the_repeating_loop.rb
+++ Module 3 - Arrays, Hashes, Blocks and Procs/the_repeating_loop.rb
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 def total(prices)
     amount = 0
     index = 0
@@ -12,6 +11,3 @@ end
 
 prices = [3.99, 25.00, 8.99]
 puts format("%.2f", total(prices)) #format .xx float
-
-=======
->>>>>>> f695b3dabd0fbb5ae32d6e8876127cc9ac882f3d
